ecbcb9e6ef41c2afce4d48b384ac4824bbff38b5
Andrew Nacin <andrewnacin@gmail.com>
2012-09-10 16:34:12 -0400

    Templating changes.  * "Duplicate of #123." rather than a bolded, starred "Marked duplicate of #123"  * "#123 was marked as a duplicate." rather than a bolded, starred "Ticket #123 was marked duplicate of this one"  * Prepend, rather than append, "Duplicate of #123" when also submitting a comment.  * Don't prepend "Duplicate of #123" if "#123" can be found in the also-submitted comment text.  * Allow '#' to be submitted along with the number withour an error, which is inevitable.

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import setup
 
 setup(
     name = 'TracDuplicates',
-    version = '0.11',
+    version = '0.11-wordpress',
     packages = ['tracduplicates'],
     author = "Nils Maier",
     author_email = "testnutzer123@gmail.com",
-    description = "Provides support for duplicates ticket references",
+    description = "Provides support for duplicates ticket references. Modified by Andrew Nacin for WordPress.",
     license = "BSD",
     keywords = "trac plugin ticket dependencies duplicates",
     url = "http://tn123.ath.cx/TracDuplicates",
     classifiers = [
         'Framework :: Trac',
     ],
-    
+
     entry_points = {
         'trac.plugins': [
             'tracduplicates.web_ui = tracduplicates.web_ui',
--- tracduplicates/web_ui.py
+++ tracduplicates/web_ui.py
@@ -1,9 +1,10 @@
 from genshi.builder import tag
 
 from trac.core import *
-from trac.ticket.api import ITicketManipulator, ITicketActionController
+from trac.ticket.api import ITicketActionController
 from trac.ticket.model import Ticket
 from trac.ticket.default_workflow import ConfigurableTicketWorkflow
+
 
 class TicketProxy:
   def __init__(self, dupe_id, ticket):
@@ -11,20 +12,19 @@ class TicketProxy:
     self._ticket = ticket
     ticket._proxy_old_save = ticket.save_changes
     ticket.save_changes = self.save_changes
-  
+
   def save_changes(self, author, comment, when=0, db=None, cnum=''):
     dupeticket = Ticket(self._ticket.env, self._dupe_id, db=db)
     dupeticket.save_changes(
       author,
-      "*** Ticket #%d marked duplicate of this one ***" % self._ticket.id,
+      "#%d was marked as a duplicate." % self._ticket.id,
       when=when,
       db=db
       )
     if not comment or not len(comment.strip()):
-      comment = ""
-    else:
-      comment += "\n\n"
-    comment += "*** Marked duplicate of #%d ***" % self._dupe_id
+      comment = "Duplicate of #%d." % self._dupe_id
+    elif -1 == comment.find('#' + str(self._dupe_id)):
+      comment = "Duplicate of #{0}.".format(self._dupe_id) + "\n\n" + comment
     return self._ticket._proxy_old_save(author, comment, when=when, db=db, cnum=cnum)
 
 class DuplicatesWorkflow(Component):
@@ -35,27 +35,27 @@ class DuplicatesWorkflow(Component):
     controller = ConfigurableTicketWorkflow(self.env)
     return controller.get_actions_by_operation_for_req(req, ticket, 'set_duplicate')
 
-  
+
   def get_all_status(self):
     return []
 
   def render_ticket_action_control(self, req, ticket, action):
-    controll = tag.input(type='text', id='action_dupe', name='action_dupe', value=req.args.get('action_dupe', ''))
-    return ('dupe to', controll, 'Mark duplicate')
+    controll = tag.input(type='text', id='action_dupe', size=5, name='action_dupe', value=req.args.get('action_dupe', '').replace('#', ''))
+    return ('dupe to', controll, '(marked duplicate)')
 
   def get_ticket_changes(self, req, ticket, action):
 
     if not req.args.get('action_dupe'):
       return {}
-  
+
     db = self.env.get_db_cnx()
     comment = req.args.get('comment')
     try:
-      dupeid = int(req.args.get('action_dupe'))
+      dupeid = int(req.args.get('action_dupe').replace('#', ''))
       Ticket(self.env, dupeid, db=db)
       TicketProxy(dupeid, ticket)
     except (ValueError, TypeError, TracError):
-      raise Exception("Invalid Duplicate Ticket Id")
+      raise Exception("Invalid ticket ID specified. Can't mark as duplicate.")
 
     return {
         'status': 'closed',
